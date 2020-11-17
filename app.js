5ac3b37ee8725abdb3a8e1fe9929a071f147ddbf
Rahul Ramteke <rahul24ramteke@gmail.com>
2020-11-17 08:12:14 +0530

    exports  date module using node exports module and refactor some code

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let workItems = [];
-let items = [];
+const workItems = [];
+const items = [];
 
 app.set('view engine', 'ejs');
 
@@ -13,22 +14,12 @@ app.use(express.static("public"));
 
 
 app.get("/", function(req, res){
-    let today = new Date();
-    
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
-    res.render("list", {listTitle: day, newListItems: items});
-
+   const day = date.getDate();
+   res.render("list", {listTitle: day, newListItems: items});
 });
 
 app.post("/", function(req, res) {
-    let item = req.body.newItem;
+    const item = req.body.newItem;
 
     if(req.body.list === "Work") {
         workItems.push(item);
@@ -44,7 +35,7 @@ app.get("/work", function(req, res){
 });
 
 app.post("/work", function(req, res){  
-    let item = req.body.newItem;
+    const item = req.body.newItem;
     workItems.push(item);
     res.redirect("/work");
 });
